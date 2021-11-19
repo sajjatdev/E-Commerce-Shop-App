b63baba6696dbd998dd94e39256905c083f18152
Md Miskatur Rahman <59787966+sajjatflutterdeveloper@users.noreply.github.com>
2021-11-19 16:02:19 +0600

    Add Text Page in this project

---
+++ lib/screen/Text.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Text extends StatelessWidget {
+  const Text({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
